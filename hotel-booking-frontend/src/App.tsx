e10c147445402a13ef2886c5fec2a59288b7ff6e
theenuka <theenukabandara@gmail.com>
2025-12-04 02:44:45 +0530

    chore: trigger CI pipeline to test SonarQube integration

--- hotel-booking-frontend/src/App.tsx
+++ hotel-booking-frontend/src/App.tsx
@@ -25,6 +25,7 @@ import AuthRedirect from "./pages/AuthRedirect";
 import useAppContext from "./hooks/useAppContext";
 
 const App = () => {
+  // Trigger CI build
   const { isLoggedIn, userRoles } = useAppContext();
   const roleSet = new Set(userRoles);
   const canManageHotels = roleSet.has("hotel_owner") || roleSet.has("admin");
